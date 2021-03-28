75ee17e24eb9689ccdbf051c6c393056b3beaafe
Alex Wong <awong5@slb.com>
2021-03-27 22:04:24 -0500

    All 2 endpoints /mars and /anne

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars anne!\n');
+  });
+
+app.get('/anne', function(req, res) {
+res.send('Hello Anne from daddy!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
